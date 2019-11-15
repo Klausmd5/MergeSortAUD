54c4e15d5bab624aa5977fbcc6b877fb542bb172
Klaus_md5 <klaus@devclan.de>
2019-11-15 11:30:03 +0100

    sort anfang

--- src/main/java/net/htlgrieskirchen/aud/mergesort/Mergesort.java
+++ src/main/java/net/htlgrieskirchen/aud/mergesort/Mergesort.java
@@ -1,4 +1,24 @@
 package net.htlgrieskirchen.aud.mergesort;
 
 public class Mergesort {
+
+    public Mergesort() {
+
+    }
+
+    public void sort(Array a) {
+
+        if(a.size() > 1) {
+            // Teilen
+            for(int i = 0; i < (a.size() / 2) -1); i++) {
+
+            }
+            Array eins = a.get(); // hälfte finden
+        } else {
+            //zusammenbauen
+        }
+
+
+    }
+
 }
